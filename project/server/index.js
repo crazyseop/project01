1da8dd5696552e162273a467362245df65b460aa
crazyseop <crazyseop@naver.com>
2022-07-28 22:15:09 +0900

    220728_2215

--- project/server/index.js
+++ project/server/index.js
@@ -1,29 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3001
-const mysql = require('mysql')
+const express = require("express");
+const app = express();
+const port = 3001;
+const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : 'eptest00',
-  database : 'userlist'
-})
+  host: "localhost",
+  user: "root",
+  password: "eptest00",
+  database: "userlist",
+});
 
-app.get('/', (req, res) => {
-  const sqlInsert = "INSERT INTO user (userID, userPassword) VALUES ('aaa', 'bbb');"
+app.get("/", (req, res) => {
+  const sqlInsert =
+    "INSERT INTO user (userID, userPassword) VALUES ('aaa', 'bbb');";
   db.query(sqlInsert, (err, result) => {
-    res.send('Hello World!')
-  })  
-})
-
-app.get('api/get', (req, res) => {
-  const sqlInsert = "SELECT * FROM user;"
-  db.query(sqlInsert, (err, result) => {
-    res.send(result);
-  })  
-})
+    res.send("Hello World!");
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
